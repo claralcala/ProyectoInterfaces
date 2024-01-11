9f48f44e007ad3f9106455b4a7bb679e43ae466a
Clara Alcalá García <calcgar312@iescarrillo.es>
2024-01-11 13:37:10 +0100

    subida_inicial

---
+++ src/main/java/ies/grupo3/interfaces/Proyecto_Interfaces/App.java
@@ -0,0 +1,13 @@
+package ies.grupo3.interfaces.Proyecto_Interfaces;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/ies/grupo3/interfaces/Proyecto_Interfaces/AppTest.java
@@ -0,0 +1,12 @@
+package ies.grupo3.interfaces.Proyecto_Interfaces;
+
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+  
+{
+   
+}
