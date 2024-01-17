1c2034174bffa248f5872f011b82184e05dffe48
ManuRguez <mrodpor884@iescarrillo.es>
2024-01-17 12:41:23 +0100

    Imagenes

--- src/main/java/vista/VentanaPrincipal.java
+++ src/main/java/vista/VentanaPrincipal.java
@@ -15,6 +15,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Font;
 import java.awt.Label;
+import java.awt.Color;
 
 public class VentanaPrincipal extends JFrame {
 
@@ -42,9 +43,12 @@ public class VentanaPrincipal extends JFrame {
 	public VentanaPrincipal() {
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 1190, 557);
+		setBounds(100, 100, 1261, 558);
 		contentPane = new JPanel();
+		contentPane.setBackground(new Color(255, 255, 255));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		
+		
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
@@ -55,29 +59,27 @@ public class VentanaPrincipal extends JFrame {
 		contentPane.add(lblNewLabel);
 		
 		JMenuBar menuBar = new JMenuBar();
+		menuBar.setBackground(new Color(255, 255, 255));
 		menuBar.setBounds(0, 0, 660, 22);
 		contentPane.add(menuBar);
 		
-		JMenu mnNewMenu = new JMenu("New menu");
+		JMenu mnNewMenu = new JMenu("Sesion");
 		menuBar.add(mnNewMenu);
 		
-		JMenuItem mntmNewMenuItem_1 = new JMenuItem("New menu item");
+		JMenuItem mntmNewMenuItem_2 = new JMenuItem("Iniciar Sesion");
+		mnNewMenu.add(mntmNewMenuItem_2);
+		
+		JMenuItem mntmNewMenuItem_1 = new JMenuItem("Registrarse");
 		mnNewMenu.add(mntmNewMenuItem_1);
 		
-		JMenuItem mntmNewMenuItem = new JMenuItem("New menu item");
+		JMenuItem mntmNewMenuItem = new JMenuItem("Salir");
 		mnNewMenu.add(mntmNewMenuItem);
 		
-		JMenu mnNewMenu_1 = new JMenu("New menu");
-		menuBar.add(mnNewMenu_1);
-		
-		JMenuItem mntmNewMenuItem_2 = new JMenuItem("New menu item");
-		mnNewMenu_1.add(mntmNewMenuItem_2);
-		
-		JMenuItem mntmNewMenuItem_3 = new JMenuItem("New menu item");
-		mnNewMenu_1.add(mntmNewMenuItem_3);
-		
-		JMenu mnNewMenu_2 = new JMenu("New menu");
+		JMenu mnNewMenu_2 = new JMenu("Version");
 		menuBar.add(mnNewMenu_2);
+		
+		JLabel lblVersion = new JLabel("<html>La aplicación <b>URBAN STRIDE</b> se encuentra en la versión 1.0</html>");
+		mnNewMenu_2.add(lblVersion);
 		
 		Button btnIniciarSesion = new Button("Iniciar Sesion");
 		btnIniciarSesion.setFont(new Font("Dialog", Font.PLAIN, 14));
@@ -85,7 +87,7 @@ public class VentanaPrincipal extends JFrame {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		btnIniciarSesion.setBounds(774, 417, 131, 22);
+		btnIniciarSesion.setBounds(776, 434, 131, 22);
 		contentPane.add(btnIniciarSesion);
 		
 		Button btnRegister = new Button("Registrarse");
@@ -94,7 +96,7 @@ public class VentanaPrincipal extends JFrame {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		btnRegister.setBounds(983, 417, 131, 22);
+		btnRegister.setBounds(986, 434, 131, 22);
 		contentPane.add(btnRegister);
 		
 		Button btnSalir = new Button("Salir");
@@ -103,16 +105,18 @@ public class VentanaPrincipal extends JFrame {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		btnSalir.setBounds(860, 469, 187, 22);
+		btnSalir.setBounds(868, 487, 187, 22);
 		contentPane.add(btnSalir);
 		
 		JLabel lblNewLabel_1 = new JLabel("New label");
 		lblNewLabel_1.setIcon(new ImageIcon(VentanaPrincipal.class.getResource("/imagenes/logo (1).png")));
-		lblNewLabel_1.setBounds(826, 0, 221, 204);
+		lblNewLabel_1.setBounds(834, 0, 221, 195);
 		contentPane.add(lblNewLabel_1);
 		
-		JLabel lblNewLabel_2 = new JLabel("Bienvenido a Urban Stride, tu destino para zapatillas exclusivas y estilo inigualable! Sumérgete en el mundo de la moda urbana con nuestra amplia selección de calzado único que fusiona la comodidad con el diseño vanguardista.\r\n\r\nEn Urban Stride, no solo vendemos zapatillas; ofrecemos una experiencia. Cada par cuenta una historia de innovación y autenticidad, cuidadosamente seleccionado para satisfacer los gustos más exigentes.\r\n\r\nExplora nuestra colección y descubre la fusión perfecta entre la moda urbana y la funcionalidad. En Urban Stride, no solo caminas, ¡defines tu propio estilo con cada paso!\r\n\r\n¡Bienvenido a la revolución del calzado urbano! Urban Stride, donde la exclusividad y la moda se encuentran en cada esquina de la ciudad.");
-		lblNewLabel_2.setBounds(670, 204, 499, 180);
+		JLabel lblNewLabel_2 = new JLabel("<html>Bienvenido a Urban Stride, tu destino para zapatillas exclusivas y estilo inigualable! Sumérgete en el mundo de la moda urbana con nuestra amplia selección de calzado único que fusiona la comodidad con el diseño vanguardista.<br><br>En Urban Stride, no solo vendemos zapatillas; ofrecemos una experiencia. Cada par cuenta una historia de innovación y autenticidad, cuidadosamente seleccionado para satisfacer los gustos más exigentes.<br><br>Explora nuestra colección y descubre la fusión perfecta entre la moda urbana y la funcionalidad. En Urban Stride, no solo caminas, ¡defines tu propio estilo con cada paso!<br><br>¡Bienvenido a la revolución del calzado urbano! Urban Stride, donde la exclusividad y la moda se encuentran en cada esquina de la ciudad.</html>");
+		lblNewLabel_2.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblNewLabel_2.setBounds(715, 193, 499, 222);
 		contentPane.add(lblNewLabel_2);
+
 	}
 }
