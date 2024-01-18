d807ff66ecce45814904e0d96e16f5e9416fac5e
mperval <mperval735@iescarrillo.es>
2024-01-18 12:57:37 +0100

    Login V3

--- src/main/java/vista/Login.java
+++ src/main/java/vista/Login.java
@@ -18,6 +18,7 @@ import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
 import java.awt.event.MouseMotionAdapter;
+import javax.swing.ImageIcon;
 
 public class Login extends JFrame {
 
@@ -52,7 +53,7 @@ public class Login extends JFrame {
 	public Login() {
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 986, 615);
+		setBounds(100, 100, 822, 654);
 		
 		setUndecorated(true);  // Quitar la barra de título y la decoración (barra superior del programa).
 		
@@ -66,8 +67,8 @@ public class Login extends JFrame {
 		
 		JPanel panel = new JPanel();
 		
-		panel.setBackground(new Color(30, 33, 57));
-		panel.setBounds(0, 0, 986, 615);
+		panel.setBackground(new Color(13, 34, 6));
+		panel.setBounds(0, 0, 822, 654);
 		contentPane.add(panel);
 		panel.setLayout(null);
 		
@@ -75,13 +76,13 @@ public class Login extends JFrame {
 		lblInicioSesion.setHorizontalAlignment(SwingConstants.CENTER);
 		lblInicioSesion.setForeground(new Color(243, 235, 219));
 		lblInicioSesion.setFont(new Font("Roboto", Font.PLAIN, 23));
-		lblInicioSesion.setBounds(371, 79, 226, 33);
+		lblInicioSesion.setBounds(293, 42, 226, 33);
 		panel.add(lblInicioSesion);
 		
 		JLabel lblUsuario = new JLabel("Usuario");
 		lblUsuario.setForeground(new Color(243, 235, 219));
 		lblUsuario.setFont(new Font("Roboto Medium", Font.PLAIN, 20));
-		lblUsuario.setBounds(274, 158, 177, 33);
+		lblUsuario.setBounds(233, 291, 177, 33);
 		panel.add(lblUsuario);
 		
 		txtIngreseUsuario = new JTextField();
@@ -100,28 +101,28 @@ public class Login extends JFrame {
 			}
 		});
 		txtIngreseUsuario.setForeground(new Color(243, 235, 219));
-		txtIngreseUsuario.setBackground(new Color(30, 33, 57));
+		txtIngreseUsuario.setBackground(new Color(10, 27, 5));
 		txtIngreseUsuario.setBorder(null);
 		txtIngreseUsuario.setText("Ingrese su nombre de usuario");
 		txtIngreseUsuario.setFont(new Font("Roboto Light", Font.PLAIN, 15));
-		txtIngreseUsuario.setBounds(274, 201, 390, 22);
+		txtIngreseUsuario.setBounds(233, 334, 390, 22);
 		panel.add(txtIngreseUsuario);
 		txtIngreseUsuario.setColumns(10);
 		
 		JSeparator separator1 = new JSeparator();
-		separator1.setBackground(new Color(155, 253, 202));
-		separator1.setBounds(274, 233, 390, 8);
+		separator1.setBackground(new Color(243, 235, 219));
+		separator1.setBounds(233, 366, 390, 8);
 		panel.add(separator1);
 		
 		lblContrasea = new JLabel("Contraseña");
 		lblContrasea.setForeground(new Color(243, 235, 219));
 		lblContrasea.setFont(new Font("Roboto Medium", Font.PLAIN, 20));
-		lblContrasea.setBounds(274, 251, 177, 24);
+		lblContrasea.setBounds(233, 384, 177, 24);
 		panel.add(lblContrasea);
 		
 		JSeparator separator1_1 = new JSeparator();
-		separator1_1.setBackground(new Color(155, 253, 202));
-		separator1_1.setBounds(274, 317, 390, 8);
+		separator1_1.setBackground(new Color(243, 235, 219));
+		separator1_1.setBounds(233, 450, 390, 8);
 		panel.add(separator1_1);
 		
 		pwdcontra = new JPasswordField();
@@ -144,15 +145,15 @@ public class Login extends JFrame {
 		pwdcontra.setForeground(new Color(243, 235, 219));
 		pwdcontra.setToolTipText("");
 		pwdcontra.setFont(new Font("Roboto Light", Font.PLAIN, 15));
-		pwdcontra.setBackground(new Color(30, 33, 57));
+		pwdcontra.setBackground(new Color(10, 27, 5));
 		pwdcontra.setText("****************");
-		pwdcontra.setBounds(274, 285, 390, 22);
+		pwdcontra.setBounds(233, 418, 390, 22);
 		pwdcontra.setBorder(null);
 		panel.add(pwdcontra);
 		
 		panelEntrar = new JPanel();
 		panelEntrar.setBackground(new Color(254, 250, 192));
-		panelEntrar.setBounds(447, 523, 113, 33);
+		panelEntrar.setBounds(347, 576, 113, 33);
 		panel.add(panelEntrar);
 		panelEntrar.setLayout(null);
 		
@@ -162,7 +163,7 @@ public class Login extends JFrame {
 		lblEntrar.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseEntered(MouseEvent e) {
-				Color colorFondo = new Color(155, 253, 202);
+				Color colorFondo = Color.green;
 				panelEntrar.setBackground(colorFondo);
 				lblEntrar.setForeground(Color.BLACK);
 			}
@@ -191,7 +192,7 @@ public class Login extends JFrame {
 		lblEntrar.setForeground(new Color(0, 64, 128));
 		
 		panelX = new JPanel();
-		panelX.setBackground(new Color(30, 33, 57));
+		panelX.setBackground(new Color(10, 27, 5));
 		panelX.setBounds(0, 0, 33, 45);
 		panel.add(panelX);
 		panelX.setLayout(null);
@@ -212,7 +213,7 @@ public class Login extends JFrame {
 			// cuando sale el raton vuelve al color original.
 			@Override
 			public void mouseExited(MouseEvent e) {
-				Color colorFondo = new Color(35, 33, 45);
+				Color colorFondo = new Color(10, 27, 5);
 				panelX.setBackground(colorFondo);
 			}
 		});
@@ -233,7 +234,7 @@ public class Login extends JFrame {
 			@Override
 			public void mouseEntered(MouseEvent e) {
 				 lblNoTeSabes.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-				 lblNoTeSabes.setForeground(Color.cyan);
+				 lblNoTeSabes.setForeground(Color.green);
 			}
 			@Override
 			public void mouseExited(MouseEvent e) {
@@ -243,17 +244,17 @@ public class Login extends JFrame {
 		lblNoTeSabes.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNoTeSabes.setForeground(new Color(243, 235, 219));
 		lblNoTeSabes.setFont(new Font("Roboto", Font.PLAIN, 12));
-		lblNoTeSabes.setBounds(371, 401, 229, 33);
+		lblNoTeSabes.setBounds(293, 468, 229, 33);
 		panel.add(lblNoTeSabes);
 		
 		JSeparator separator1_1_1 = new JSeparator();
 		separator1_1_1.setBackground(new Color(255, 255, 255));
-		separator1_1_1.setBounds(371, 426, 226, 8);
+		separator1_1_1.setBounds(302, 500, 217, 8);
 		panel.add(separator1_1_1);
 		
 		final JPanel panel_ = new JPanel();
 		panel_.setLayout(null);
-		panel_.setBackground(new Color(30, 33, 57));
+		panel_.setBackground(new Color(10, 27, 5));
 		panel_.setBounds(35, 0, 33, 45);
 		panel.add(panel_);
 		
@@ -269,14 +270,25 @@ public class Login extends JFrame {
 			}
 			@Override
 			public void mouseExited(MouseEvent e) {
-				Color colorFondo = new Color(35, 33, 45);
+				Color colorFondo = new Color(10, 27, 5);
 				panel_.setBackground(colorFondo);
 			}
 		});
 		lbl_.setHorizontalAlignment(SwingConstants.CENTER);
+		lbl_.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR)); // cursor que cuando pasa por encima cambia al icono de la mano en el raton.
 		lbl_.setForeground(new Color(216, 200, 187));
 		lbl_.setFont(new Font("Roboto Black", Font.PLAIN, 20));
 		lbl_.setBounds(0, 0, 33, 45);
 		panel_.add(lbl_);
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon(Login.class.getResource("/imagenes/logo (1).png")));
+		lblNewLabel.setBounds(293, 85, 240, 196);
+		panel.add(lblNewLabel);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(new Color(10, 27, 5));
+		panel_1.setBounds(190, 25, 469, 601);
+		panel.add(panel_1);
 	}
 }
